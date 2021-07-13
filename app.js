80a7d569657a6eddc277a9a6f56df8b630db2120
Taís Foltran Orsini <taisfoltranorsini.com>
2021-07-13 09:23:28 -0300

    done

--- app.js
+++ app.js
@@ -9,4 +9,41 @@ const app = express();
 
 // -- Define your route listeners here! --
 
+app.get("/pokemon", (req, res) => {
+  return res.json(allPokemon);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  const id = req.params.id;
+  const foundPokemon = allPokemon.find((pokemon) => {
+    return pokemon.id === Number(id);
+  });
+
+  if (foundPokemon) {
+    return res.json(foundPokemon);
+  } else {
+    return res.json({ msg: "Pokemon not found." });
+  }
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const queryParams = req.query;
+
+  for (let key in queryParams) {
+    const foundPokemon = allPokemon.find((pokemon) => {
+      return pokemon[key]
+        .toLowerCase()
+        .includes(queryParams[key].toLowerCase());
+    });
+
+    if (foundPokemon) {
+      return res.json(foundPokemon);
+    } else {
+      return res.json({ msg: "Pokemon not found." });
+    }
+  }
+
+  res.json(queryParams);
+});
+
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
